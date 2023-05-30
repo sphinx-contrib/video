0c5d54cf5b16584dee4a3649246674c9288707cc
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-05-30 20:16:12 +0200

    bump: version 0.1.1 → 0.2.0

--- sphinxcontrib/video.py
+++ sphinxcontrib/video.py
@@ -12,7 +12,7 @@ from sphinx.util import logging
 from sphinx.util.docutils import SphinxDirective, SphinxTranslator
 
 __author__ = "Raphael Massabot"
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 logger = logging.getLogger(__name__)
 
