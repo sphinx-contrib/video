f84d6b2711b4bb2fea12f297456eae7782313e77
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-11-11 08:17:14 +0000

    bump: version 0.3.0 → 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "sphinxcontrib-video"
 author = "Raphael Massabot"
 copyright = f"2018-{datetime.now().year}, {author}"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration -----------------------------------------------------
 
